3d66442a97a9736cdb4b3076055fa7d76a212c94
Duilan <soyduilan@gmail.com>
2020-12-17 21:46:19 -0600

    Initial Commit

---
+++ RickAndMorty Characters iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty Characters iOS
+//
+//  Created by Duilan on 17/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
