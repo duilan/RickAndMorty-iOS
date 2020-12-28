ac35d5ccf4c5c8912d7bcd2b29e76ee66f1e4cd9
Duilan <soyduilan@gmail.com>
2020-12-27 22:51:10 -0600

    Remove touch animation

--- RickAndMorty Characters iOS/Modules/CharacterList/Cell/CharacterTableViewCell.swift
+++ RickAndMorty Characters iOS/Modules/CharacterList/Cell/CharacterTableViewCell.swift
@@ -76,35 +76,3 @@ final class CharacterTableViewCell: UITableViewCell {
         self.layer.shadowRadius = shadowRadius
     }
 }
-
-// MARK: - ANIMATE TOUCH CELL
-extension CharacterTableViewCell {
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        DispatchQueue.main.async {
-            self.card.transform  = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            UIView.animate(withDuration: self.durationTransformCard, delay: 0.0, options: .curveLinear, animations: {
-                self.card.transform  = CGAffineTransform(scaleX: self.scaleTransformCard, y: self.scaleTransformCard)
-            }, completion: nil)
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        DispatchQueue.main.async {
-            self.card.transform  = CGAffineTransform(scaleX: self.scaleTransformCard, y: self.scaleTransformCard)
-            UIView.animate(withDuration: self.durationTransformCard, delay: 0.0, options: .curveLinear, animations: {
-                self.card.transform  = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            }, completion: nil)
-        }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        DispatchQueue.main.async {
-            self.card.transform  = CGAffineTransform(scaleX: self.scaleTransformCard, y: self.scaleTransformCard)
-            UIView.animate(withDuration: self.durationTransformCard, delay: 0.0, options: .curveLinear, animations: {
-                self.card.transform  = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            }, completion: nil)
-        }
-    }
-    
-}
